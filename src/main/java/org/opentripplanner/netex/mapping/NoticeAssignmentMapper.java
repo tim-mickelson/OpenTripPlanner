99af5dd669ccab87f362f42c7e547f5085a18a32
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2017-11-14 11:43:21 +0100

    Handle journeyPattern not found when mapping notices

--- src/main/java/org/opentripplanner/netex/mapping/NoticeAssignmentMapper.java
+++ src/main/java/org/opentripplanner/netex/mapping/NoticeAssignmentMapper.java
@@ -4,6 +4,8 @@ import org.opentripplanner.model.NoticeAssignment;
 import org.opentripplanner.netex.loader.NetexImportDataIndex;
 import org.rutebanken.netex.model.JourneyPattern;
 import org.rutebanken.netex.model.ServiceJourney;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,28 +16,41 @@ import static org.opentripplanner.netex.mapping.FeedScopedIdFactory.createFeedSc
 
 public class NoticeAssignmentMapper {
 
+    private static final Logger LOG = LoggerFactory.getLogger(NoticeAssignmentMapper.class);
+
     public Collection<NoticeAssignment> mapNoticeAssignment(org.rutebanken.netex.model.NoticeAssignment netexNoticeAssignment, NetexImportDataIndex netexIndex){
         Collection<NoticeAssignment> noticeAssignments = new ArrayList<>();
+        String journeyPatternRef = netexNoticeAssignment.getNoticedObjectRef().getRef();
 
         if (getObjectType(netexNoticeAssignment).equals("StopPointInJourneyPattern")) {
-            JourneyPattern journeyPattern = netexIndex.journeyPatternsByStopPointId.lookup(netexNoticeAssignment.getNoticedObjectRef().getRef());
+            JourneyPattern journeyPattern = netexIndex.journeyPatternsByStopPointId.lookup(journeyPatternRef);
 
-            // Map notice from StopPointInJourneyPattern to corresponding TimeTabledPassingTimes
-            for (ServiceJourney serviceJourney : netexIndex.serviceJourneyByPatternId.lookup(journeyPattern.getId())) {
-                org.opentripplanner.model.NoticeAssignment otpNoticeAssignment = new org.opentripplanner.model.NoticeAssignment();
+            if (journeyPattern != null) {
+                boolean serviceJourneyNotFound = true;
+                // Map notice from StopPointInJourneyPattern to corresponding TimeTabledPassingTimes
+                for (ServiceJourney serviceJourney : netexIndex.serviceJourneyByPatternId.lookup(journeyPattern.getId())) {
+                    serviceJourneyNotFound = false;
+                    org.opentripplanner.model.NoticeAssignment otpNoticeAssignment = new org.opentripplanner.model.NoticeAssignment();
 
-                otpNoticeAssignment.setId(createFeedScopedId(netexNoticeAssignment.getId()));
-                otpNoticeAssignment.setNoticeId(createFeedScopedId(netexNoticeAssignment.getNoticeRef().getRef()));
-                otpNoticeAssignment.setElementId(createFeedScopedId(serviceJourney.getId()));
+                    otpNoticeAssignment.setId(createFeedScopedId(netexNoticeAssignment.getId()));
+                    otpNoticeAssignment.setNoticeId(createFeedScopedId(netexNoticeAssignment.getNoticeRef().getRef()));
+                    otpNoticeAssignment.setElementId(createFeedScopedId(serviceJourney.getId()));
 
-                noticeAssignments.add(otpNoticeAssignment);
+                    noticeAssignments.add(otpNoticeAssignment);
+                }
+                if(serviceJourneyNotFound){
+                    LOG.warn("ServiceJourney for journeyPatternRef " + journeyPatternRef + " not found when mapping notices.");
+                }
+            }
+            else {
+                LOG.warn("JourneyPattern " + journeyPatternRef + " not found when mapping notices.");
             }
         } else {
             org.opentripplanner.model.NoticeAssignment otpNoticeAssignment = new org.opentripplanner.model.NoticeAssignment();
 
             otpNoticeAssignment.setId(createFeedScopedId(netexNoticeAssignment.getId()));
             otpNoticeAssignment.setNoticeId(createFeedScopedId(netexNoticeAssignment.getNoticeRef().getRef()));
-            otpNoticeAssignment.setElementId(createFeedScopedId(netexNoticeAssignment.getNoticedObjectRef().getRef()));
+            otpNoticeAssignment.setElementId(createFeedScopedId(journeyPatternRef));
 
             noticeAssignments.add(otpNoticeAssignment);
         }
