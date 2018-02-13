b390ffe16221779247cc051939a095fad065f571
Thomas Gran <tgr@capraconsulting.no>
2018-02-13 16:46:09 +0100

    Set default stopClusterMode to "parentStation" (not "proximity"), to fix error. The cluster Stops are generated BEFORE the routing config is loaded, and the build config is ignored. Unfortunately this code is a mess, and fixing it would take some time.

--- src/main/java/org/opentripplanner/routing/graph/Graph.java
+++ src/main/java/org/opentripplanner/routing/graph/Graph.java
@@ -230,7 +230,7 @@ public class Graph implements Serializable, AddBuilderAnnotation {
     public transient StreetSpeedSnapshotSource streetSpeedSource;
     
     /** How should we cluster stops? */
-    public String stopClusterMode = "proximity";
+    public String stopClusterMode = "parentStation";
 
     /** The difference in meters between the WGS84 ellipsoid height and geoid height at the graph's center */
     public Double ellipsoidToGeoidDifference = 0.0;
