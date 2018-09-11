f4807acd0ba1aabf51ee04199916da05e704e03d
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-09-11 15:49:17 +0200

    ROR-500 Set properties for protostuff deserializing

--- src/main/java/org/opentripplanner/serializer/ProtostuffGraphDeserializer.java
+++ src/main/java/org/opentripplanner/serializer/ProtostuffGraphDeserializer.java
@@ -1,11 +1,14 @@
 package org.opentripplanner.serializer;
 
+import com.google.common.io.ByteStreams;
+import io.protostuff.CodedInput;
 import io.protostuff.GraphIOUtil;
 import io.protostuff.Schema;
 import io.protostuff.runtime.RuntimeSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,14 +25,22 @@ public class ProtostuffGraphDeserializer implements GraphDeserializer {
     @Override
     public GraphWrapper deserialize(InputStream inputStream) {
 
-        final long started = System.currentTimeMillis();
-        LOG.debug("Creating schema");
-        // Can be cached
-        Schema<GraphWrapper> schema = RuntimeSchema.getSchema(GraphWrapper.class);
-        LOG.debug("Deserializing");
+        // Set these elsewhere
+        System.setProperty("protostuff.runtime.collection_schema_on_repeated_fields", "true");
+        System.setProperty("protostuff.runtime.morph_non_final_pojos", "true");
+        System.setProperty("protostuff.runtime.allow_null_array_element", "true");
 
-        GraphWrapper graphWrapperFromProtostuff = schema.newMessage();
+
         try {
+
+            final long started = System.currentTimeMillis();
+            LOG.debug("Creating schema");
+            // Can be cached
+            Schema<GraphWrapper> schema = RuntimeSchema.getSchema(GraphWrapper.class);
+
+            LOG.debug("Deserializing");
+
+            GraphWrapper graphWrapperFromProtostuff = schema.newMessage();
             GraphIOUtil.mergeFrom(inputStream, graphWrapperFromProtostuff, schema);
             LOG.debug("Returning wrapped graph object after {} ms", System.currentTimeMillis() - started);
             return graphWrapperFromProtostuff;
