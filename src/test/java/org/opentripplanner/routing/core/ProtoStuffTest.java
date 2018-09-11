93274fb7e21cf9c7fafa5cf5377cdb416180f5f1
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2018-09-11 10:49:06 +0200

    Allow null elements in arrays

--- src/test/java/org/opentripplanner/routing/core/ProtoStuffTest.java
+++ src/test/java/org/opentripplanner/routing/core/ProtoStuffTest.java
@@ -106,6 +106,7 @@ public class ProtoStuffTest extends TestCase {
 
         System.setProperty("protostuff.runtime.collection_schema_on_repeated_fields", "true");
         System.setProperty("protostuff.runtime.morph_non_final_pojos", "true");
+        System.setProperty("protostuff.runtime.allow_null_array_element", "true");
 
         long schemaStarted = System.currentTimeMillis();
         Schema<EdgeInfo> schema = RuntimeSchema.getSchema(EdgeInfo.class);
