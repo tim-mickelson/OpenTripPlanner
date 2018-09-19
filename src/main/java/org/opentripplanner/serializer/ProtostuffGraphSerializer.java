8b477018547f43c43179bca084079f9aedd464e5
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-09-19 12:34:58 +0200

    Add the consolitdating edges code to protostuff as well

--- src/main/java/org/opentripplanner/serializer/ProtostuffGraphSerializer.java
+++ src/main/java/org/opentripplanner/serializer/ProtostuffGraphSerializer.java
@@ -2,16 +2,21 @@ package org.opentripplanner.serializer;
 
 import io.protostuff.*;
 import io.protostuff.runtime.RuntimeSchema;
+import org.opentripplanner.routing.graph.Edge;
+import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.routing.graph.Vertex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProtostuffGraphSerializer implements GraphSerializer {
 
-    private static final int SIZE_LIMIT = 2000000000;
+    private static final long SIZE_LIMIT = Long.MAX_VALUE;
     private static final Logger LOG = LoggerFactory.getLogger(ProtostuffGraphSerializer.class);
 
     private final Schema<GraphWrapper> schema;
@@ -34,16 +39,16 @@ public class ProtostuffGraphSerializer implements GraphSerializer {
 
             final long started = System.currentTimeMillis();
 
-            GraphWrapper graphWrapperFromProtostuff = schema.newMessage();
+            GraphWrapper deserializedGraph = schema.newMessage();
 
             CodedInput input = new CodedInput(inputStream, true);
-            input.setSizeLimit(SIZE_LIMIT);
+            input.setSizeLimit(Long.MAX_VALUE);
             GraphCodedInput graphInput = new GraphCodedInput(input);
-            schema.mergeFrom(graphInput, graphWrapperFromProtostuff);
+            schema.mergeFrom(graphInput, deserializedGraph);
             input.checkLastTagWas(0);
 
             LOG.debug("Returning wrapped graph object after {} ms", System.currentTimeMillis() - started);
-            return graphWrapperFromProtostuff;
+            return deserializedGraph;
         } catch (IOException e) {
             throw new GraphSerializationException("Cannot deserialize graph", e);
         }
@@ -56,12 +61,30 @@ public class ProtostuffGraphSerializer implements GraphSerializer {
         LinkedBuffer linkedBuffer = LinkedBuffer.allocate(linkedBufferSize);
 
         try {
+
+            Graph graph = graphWrapper.graph;
+
+            LOG.debug("Consolidating edges...");
+            // this is not space efficient
+            List<Edge> edges = new ArrayList<Edge>(graph.countEdges());
+            for (Vertex v : graph.getVertices()) {
+                // there are assumed to be no edges in an incoming list that are not
+                // in an outgoing list
+                edges.addAll(v.getOutgoing());
+                if (v.getDegreeOut() + v.getDegreeIn() == 0)
+                    LOG.debug("vertex {} has no edges, it will not survive serialization.", v);
+            }
+            LOG.debug("Assigning vertex/edge ID numbers...");
+            graph.rebuildVertexAndEdgeIndices();
+            graphWrapper.edges = edges;
+
+
             int written = GraphIOUtil.writeTo(outputStream, graphWrapper, schema, linkedBuffer);
             long millisSpent = System.currentTimeMillis() - writeToFileStarted;
-            LOG.debug("Written {} bytes to protostuff in {} ms", written, millisSpent);
+            LOG.info("Written {} bytes to protostuff in {} ms", written, millisSpent);
 
         } catch (IOException e) {
-            throw new GraphSerializationException("Cannot deserialize graph", e);
+            throw new GraphSerializationException("Cannot serialize graph", e);
         }
 
     }
