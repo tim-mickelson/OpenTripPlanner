2d1cdc528f25c92aad5b09b34ff5c174457e1cc6
Gard Mellemstrand <gard.mellemstrand@entur.org>
2018-10-15 13:10:28 +0200

    Use raptor algorithm by default

--- src/main/java/org/opentripplanner/routing/core/RoutingRequest.java
+++ src/main/java/org/opentripplanner/routing/core/RoutingRequest.java
@@ -502,7 +502,7 @@ public class RoutingRequest implements Cloneable, Serializable {
     /**
      * Use the Raptor algorithm for route planning.
      */
-    public boolean useRaptorAlgorithm = false;
+    public boolean useRaptorAlgorithm = true;
 
     /** Saves split edge which can be split on origin/destination search
      *
