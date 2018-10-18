1024aa7949002183cb29a05ba0b26242870fbc90
Gard Mellemstrand <gard.mellemstrand@entur.org>
2018-10-18 12:31:48 +0200

    Set max transfer to 7 by default

--- src/main/java/org/opentripplanner/routing/core/RoutingRequest.java
+++ src/main/java/org/opentripplanner/routing/core/RoutingRequest.java
@@ -331,7 +331,7 @@ public class RoutingRequest implements Cloneable, Serializable {
 
     public int alightSlack = 0;
 
-    public int maxTransfers = 2;
+    public int maxTransfers = 7;
 
     /**
      * Extensions to the trip planner will require additional traversal options beyond the default 
