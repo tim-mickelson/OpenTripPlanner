9ca24756e30467e02f19fbca4d7f59a630f5d0a8
Thomas Gran <t2gran@gmail.com>
2019-01-11 19:25:05 +0100

    Recalculate the the elevation profile for temporary edges after a split. This apply to biking and walking.

--- src/main/java/org/opentripplanner/graph_builder/linking/SimpleStreetSplitter.java
+++ src/main/java/org/opentripplanner/graph_builder/linking/SimpleStreetSplitter.java
@@ -418,9 +418,7 @@ public class SimpleStreetSplitter {
                     splitPoint.x, splitPoint.y, edge);
         }
 
-        // make the edges
-        // TODO this is using the StreetEdge implementation of split, which will discard elevation information
-        // on edges that have it
+        // make the edges on edges that have it
         P2<StreetEdge> edges = edge.split(v, !temporarySplit);
 
         if (destructiveSplitting) {
