42e9ab73badc04615946606b46178d593fee5c30
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-01-17 15:37:02 +0100

    Make flex searches start at the first quay

--- src/main/java/org/opentripplanner/index/transmodel/TransmodelGraphQLPlanner.java
+++ src/main/java/org/opentripplanner/index/transmodel/TransmodelGraphQLPlanner.java
@@ -295,6 +295,9 @@ public class TransmodelGraphQLPlanner {
             request.to.vertexId = getLocationOfFirstQuay(request.to.vertexId, ((Router)environment.getContext()).graph.index);
         } else if (request.parkAndRide) {
             request.from.vertexId = getLocationOfFirstQuay(request.from.vertexId, ((Router)environment.getContext()).graph.index);
+        } else if (request.useFlexService) {
+            request.from.vertexId = getLocationOfFirstQuay(request.from.vertexId, ((Router)environment.getContext()).graph.index);
+            request.to.vertexId = getLocationOfFirstQuay(request.to.vertexId, ((Router)environment.getContext()).graph.index);
         }
 
         return request;
