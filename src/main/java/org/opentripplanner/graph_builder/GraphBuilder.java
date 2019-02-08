4dbd3e5d801002f463966fa11ef621b6d3b42186
Assad Riaz <assad.riaz@bouvet.no>
2019-02-08 14:59:46 +0100

    Updated BASE_GRAPH_FILENAME to baseGraphX.obj

--- src/main/java/org/opentripplanner/graph_builder/GraphBuilder.java
+++ src/main/java/org/opentripplanner/graph_builder/GraphBuilder.java
@@ -68,9 +68,9 @@ public class GraphBuilder implements Runnable {
 
     public static final String GRAPH_FILENAME = "Graph.obj";
 
-    public static final String BASE_GRAPH_FILENAME = "baseGraph.obj";
+    public static final String BASE_GRAPH_FILENAME = "baseGraphX.obj";
 
-    private List<GraphBuilderModule> _graphBuilderModules = new ArrayList<GraphBuilderModule>();
+    private List<GraphBuilderModule> _graphBuilderModules = new ArrayList<>();
 
     private File graphFile;
     
