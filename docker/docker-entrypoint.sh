e169eddc491e8ebf40ca261fb69cad2d9d087aca
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-03-05 14:14:03 +0100

    Deleted router-config

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 : ${GRAPH_FILE_TARGET_PATH="/code/otpdata/norway/Graph.obj"}
 : ${FILENAME="https://storage.googleapis.com/marduk-production/graphs/1.3.4.RB-SNAPSHOT/netex-20190228041737-Graph.obj"}
-: ${ROUTER_CONFIG_TARGET_PATH="/code/otpdata/norway/Graph.obj"}
+: ${ROUTER_CONFIG_TARGET_PATH="/code/otpdata/norway/router-config.json"}
 : ${ROUTER_CONFIG_URL="https://storage.googleapis.com/marduk/graphs/neon-graph/router-config.json"}
 
 echo "GRAPH_FILE_TARGET_PATH: $GRAPH_FILE_TARGET_PATH"
