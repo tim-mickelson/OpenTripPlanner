trnovstyle/OpenTripPlanner-zmaj

Get only latest version of quay

src/main/java/org/opentripplanner/netex/mapping/StopMapper.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.netex.mapping;
 
 import com.google.common.collect.Iterables;
+import org.opentripplanner.graph_builder.model.NetexDao;
 import org.opentripplanner.model.AgencyAndId;
 import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.impl.OtpTransitDaoBuilder;
@@ -19,7 +20,7 @@ import java.util.stream.Collectors;
 public class StopMapper {
     private static final Logger LOG = LoggerFactory.getLogger(StopMapper.class);
 
-    public Collection<Stop> mapParentAndChildStops(Collection<StopPlace> stopPlaceAllVersions, OtpTransitDaoBuilder transitBuilder){
+    public Collection<Stop> mapParentAndChildStops(Collection<StopPlace> stopPlaceAllVersions, OtpTransitDaoBuilder transitBuilder, NetexDao netexDao){
         ArrayList<Stop> stops = new ArrayList<>();
 
         Stop multiModalStop = null;
@@ -28,7 +29,7 @@ public class StopMapper {
 
         // Sort by versions, latest first
         stopPlaceAllVersions = stopPlaceAllVersions.stream()
-                .sorted((o1, o2) -> Integer.compare(Integer.parseInt(o2.getVersion()), Integer.parseInt(o1.getVersion())))
+                .sorted((o1, o2) -> Integer.compare(Integer.parseInt(o1.getVersion()), Integer.parseInt(o2.getVersion())))
                 .collect(Collectors.toList());
 
         StopPlace stopPlaceLatest = Iterables.getLast(stopPlaceAllVersions);
@@ -89,6 +90,13 @@ public class StopMapper {
                         stopQuay.setParentStation(stop.getId().getId());
                         if (multiModalStop != null) {
                             stopQuay.setMultiModalStation(multiModalStop.getId().getId());
+                        }
+
+
+                        // Continue if this is not newest version of quay
+                        if (netexDao.getQuayById().get(stopQuay.getId().getId().toString()).stream()
+                                .anyMatch(q -> Integer.parseInt(q.getVersion()) > Integer.parseInt(quay.getVersion()))) {
+                            continue;
                         }
 
                         if (!quaysSeen.contains(quay.getId())) {

tim-mickelson/OpenTripPlanner

src/main/java/org/opentripplanner/netex/mapping/StopMapper.java
@@ -2,6 +2,7 @@ package org.opentripplanner.netex.mapping;
 
 import com.google.common.collect.Iterables;
 import org.opentripplanner.model.Stop;
+import org.opentripplanner.netex.loader.NetexDao;
 import org.rutebanken.netex.model.Quay;
 import org.rutebanken.netex.model.StopPlace;
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,7 +19,7 @@ import java.util.stream.Collectors;
 public class StopMapper {
     private static final Logger LOG = LoggerFactory.getLogger(StopMapper.class);
 
-    public Collection<Stop> mapParentAndChildStops(Collection<StopPlace> stopPlaceAllVersions){
+    public Collection<Stop> mapParentAndChildStops(Collection<StopPlace> stopPlaceAllVersions, NetexDao netexDao){
         ArrayList<Stop> stops = new ArrayList<>();
 
         Stop stop = new Stop();
@@ -25,7 +27,7 @@ public class StopMapper {
 
         // Sort by versions, latest first
         stopPlaceAllVersions = stopPlaceAllVersions.stream()
-                .sorted((o1, o2) -> Integer.compare(Integer.parseInt(o2.getVersion()), Integer.parseInt(o1.getVersion())))
+                .sorted(Comparator.comparingInt(o -> Integer.parseInt(o.getVersion())))
                 .collect(Collectors.toList());
 
         StopPlace stopPlaceLatest = Iterables.getLast(stopPlaceAllVersions);
@@ -68,6 +70,13 @@ public class StopMapper {
                         stopQuay.setLon(quay.getCentroid().getLocation().getLongitude().doubleValue());
                         stopQuay.setId(FeedScopedIdFactory.createFeedScopedId(quay.getId()));
                         stopQuay.setParentStation(stop.getId().getId());
+
+
+                        // Continue if this is not newest version of quay
+                        if (netexDao.lookupQuayById(stopQuay.getId().getId()).stream()
+                                .anyMatch(q -> Integer.parseInt(q.getVersion()) > Integer.parseInt(quay.getVersion()))) {
+                            continue;
+                        }
 
                         if (!quaysSeen.contains(quay.getId())) {
                             stops.add(stopQuay);