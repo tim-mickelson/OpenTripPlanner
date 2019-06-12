trnovstyle/OpenTripPlanner-zmaj

Improve NeTEx import with cashing reduced to only hold current elements according the the file levels: shared files -> group shared files -> independent group files. When mapping an 'independent group file' only the data in that file, the shared group file data and the shared data is available. Data from other groups or other independent group files are not available. Because the NeTEx model is extreamly verbose and memory hungry, this approach reduces the memory usage for large imports to about 1/3.

src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
@@ -1,6 +1,6 @@
 package org.opentripplanner.netex.mapping;
 
-import org.opentripplanner.graph_builder.model.NetexDao;
+import org.opentripplanner.netex.loader.NetexDao;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.impl.OtpTransitBuilder;
@@ -15,17 +15,17 @@ import static org.opentripplanner.netex.mapping.CalendarMapper.mapToCalendarDate
 
 public class NetexMapper {
 
-    final OtpTransitBuilder transitBuilder;
+    private final AgencyMapper agencyMapper = new AgencyMapper();
 
-    AgencyMapper agencyMapper = new AgencyMapper();
+    private final RouteMapper routeMapper = new RouteMapper();
 
-    RouteMapper routeMapper = new RouteMapper();
+    private final StopMapper stopMapper = new StopMapper();
 
-    StopMapper stopMapper = new StopMapper();
+    private final TripPatternMapper tripPatternMapper = new TripPatternMapper();
 
-    TripPatternMapper tripPatternMapper = new TripPatternMapper();
+    private final OtpTransitBuilder transitBuilder;
 
-    String agencyId;
+    private final String agencyId;
 
 
     public NetexMapper(OtpTransitBuilder transitBuilder, String agencyId) {
@@ -33,24 +33,20 @@ public class NetexMapper {
         this.agencyId = agencyId;
     }
 
-    public OtpTransitBuilder mapNetexToOtp(NetexDao netexDao) {
+    public void mapNetexToOtp(NetexDao netexDao) {
         AgencyAndIdFactory.setAgencyId(agencyId);
 
-        for (Operator operator : netexDao.getOperators().values()) {
-            if (operator != null) {
-                transitBuilder.getAgencies().add(agencyMapper.mapAgency(operator, "Europe/Oslo"));
-            }
+        for (Operator operator : netexDao.getOperators()) {
+            transitBuilder.getAgencies().add(agencyMapper.mapAgency(operator, "Europe/Oslo"));
         }
 
-        for (Line line : netexDao.getLineById().values()) {
-            if (line != null) {
-                Route route = routeMapper.mapRoute(line, transitBuilder);
-                transitBuilder.getRoutes().add(route);
-            }
+        for (Line line : netexDao.getLines()) {
+            Route route = routeMapper.mapRoute(line, transitBuilder);
+            transitBuilder.getRoutes().add(route);
         }
 
-        for (String stopPlaceId : netexDao.getStopsById().keySet()) {
-            Collection<StopPlace> stopPlaceAllVersions = netexDao.getStopsById().get(stopPlaceId);
+        for (String stopPlaceId : netexDao.getStopPlaceIds()) {
+            Collection<StopPlace> stopPlaceAllVersions = netexDao.lookupStopPlacesById(stopPlaceId);
             if (stopPlaceAllVersions != null) {
                 Collection<Stop> stops = stopMapper.mapParentAndChildStops(stopPlaceAllVersions);
                 for (Stop stop : stops) {
@@ -59,16 +55,12 @@ public class NetexMapper {
             }
         }
 
-        for (JourneyPattern journeyPattern : netexDao.getJourneyPatternsById().values()) {
-            if (journeyPattern != null) {
-                tripPatternMapper.mapTripPattern(journeyPattern, transitBuilder, netexDao);
-            }
+        for (JourneyPattern journeyPattern : netexDao.getJourneyPatterns()) {
+            tripPatternMapper.mapTripPattern(journeyPattern, transitBuilder, netexDao);
         }
 
-        for (String serviceId : netexDao.getServiceIds().values()) {
+        for (String serviceId : netexDao.getCalendarServiceIds()) {
             transitBuilder.getCalendarDates().addAll(mapToCalendarDates(AgencyAndIdFactory.createAgencyAndId(serviceId), netexDao));
         }
-
-        return transitBuilder;
     }
 }

src/test/java/org/opentripplanner/netex/MappingTest.java
@@ -6,6 +6,8 @@ import org.joda.time.MutableDateTime;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opentripplanner.model.Agency;
+import org.opentripplanner.netex.loader.NetexLoader;
 import org.opentripplanner.gtfs.GtfsContextBuilder;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.ServiceCalendar;
@@ -13,7 +15,7 @@ import org.opentripplanner.model.ServiceCalendarDate;
 import org.opentripplanner.model.StopTime;
 import org.opentripplanner.model.calendar.ServiceDate;
 import org.opentripplanner.model.impl.OtpTransitBuilder;
-import org.opentripplanner.graph_builder.model.NetexBundle;
+import org.opentripplanner.netex.loader.NetexBundle;
 import org.opentripplanner.graph_builder.module.NetexModule;
 import org.opentripplanner.standalone.GraphBuilderParameters;
 import org.opentripplanner.standalone.OTPMain;
@@ -30,8 +32,6 @@ import java.util.stream.Collectors;
 
 
 public class MappingTest {
-
-
     static final String gtfsFile = "src/test/resources/netex_mapping_test/gtfs_minimal_fileset/gtfs_minimal.zip";
     static final File netexFile = new File("src/test/resources/netex_mapping_test/netex_minimal_fileset/netex_minimal.zip");
     static final File netexConfigFile = new File("src/test/resources/netex_mapping_test/build-config.json");
@@ -51,9 +51,20 @@ public class MappingTest {
                 add(netexBundle);
             }
         });
-        otpBuilderFromNetex = netexModule.getOtpDao().stream().findFirst().get();
-        otpBuilderFromGtfs = GtfsContextBuilder.contextBuilder(gtfsFile).turnOnSetAgencyToFeedIdForAllElements().build()
+        otpBuilderFromNetex = new NetexLoader(netexBundle).loadBundle();
+        otpBuilderFromGtfs = GtfsContextBuilder
+                .contextBuilder(gtfsFile)
+                .turnOnSetAgencyToFeedIdForAllElements().build()
                 .getTransitBuilder();
+    }
+
+    @Test
+    public void testAgencies() {
+        List<Agency> ga = otpBuilderFromGtfs.getAgencies();
+        List<Agency> na = otpBuilderFromNetex.getAgencies();
+
+        // TODO TGR - fix this test
+        Assert.assertEquals(ga.size(), na.size());
     }
 
     @Test

tim-mickelson/OpenTripPlanner

src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.netex.mapping;
 
-import org.opentripplanner.graph_builder.model.NetexDao;
+import org.opentripplanner.model.OtpTransitService;
+import org.opentripplanner.netex.loader.NetexDao;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.impl.OtpTransitServiceBuilder;
@@ -16,11 +17,12 @@ import static org.opentripplanner.netex.mapping.FeedScopedIdFactory.createFeedSc
 
 public class NetexMapper {
 
-    private final OtpTransitServiceBuilder transitBuilder;
     private final AgencyMapper agencyMapper = new AgencyMapper();
     private final RouteMapper routeMapper = new RouteMapper();
     private final StopMapper stopMapper = new StopMapper();
     private final TripPatternMapper tripPatternMapper = new TripPatternMapper();
+
+    private final OtpTransitServiceBuilder transitBuilder;
     private final String agencyId;
 
 
@@ -29,24 +31,20 @@ public class NetexMapper {
         this.agencyId = agencyId;
     }
 
-    public OtpTransitServiceBuilder mapNetexToOtp(NetexDao netexDao) {
+    public void mapNetexToOtp(NetexDao netexDao) {
         FeedScopedIdFactory.setFeedId(agencyId);
 
-        for (Operator operator : netexDao.getOperators().values()) {
-            if (operator != null) {
-                transitBuilder.getAgencies().add(agencyMapper.mapAgency(operator, "Europe/Oslo"));
-            }
+        for (Operator operator : netexDao.getOperators()) {
+            transitBuilder.getAgencies().add(agencyMapper.mapAgency(operator, "Europe/Oslo"));
         }
 
-        for (Line line : netexDao.getLineById().values()) {
-            if (line != null) {
-                Route route = routeMapper.mapRoute(line, transitBuilder);
-                transitBuilder.getRoutes().add(route);
-            }
+        for (Line line : netexDao.getLines()) {
+            Route route = routeMapper.mapRoute(line, transitBuilder);
+            transitBuilder.getRoutes().add(route);
         }
 
-        for (String stopPlaceId : netexDao.getStopsById().keySet()) {
-            Collection<StopPlace> stopPlaceAllVersions = netexDao.getStopsById().get(stopPlaceId);
+        for (String stopPlaceId : netexDao.getStopPlaceIds()) {
+            Collection<StopPlace> stopPlaceAllVersions = netexDao.lookupStopPlacesById(stopPlaceId);
             if (stopPlaceAllVersions != null) {
                 Collection<Stop> stops = stopMapper.mapParentAndChildStops(stopPlaceAllVersions);
                 for (Stop stop : stops) {
@@ -55,16 +53,14 @@ public class NetexMapper {
             }
         }
 
-        for (JourneyPattern journeyPattern : netexDao.getJourneyPatternsById().values()) {
-            if (journeyPattern != null) {
-                tripPatternMapper.mapTripPattern(journeyPattern, transitBuilder, netexDao);
-            }
+        for (JourneyPattern journeyPattern : netexDao.getJourneyPatterns()) {
+            tripPatternMapper.mapTripPattern(journeyPattern, transitBuilder, netexDao);
         }
 
-        for (String serviceId : netexDao.getServiceIds().values()) {
-            transitBuilder.getCalendarDates().addAll(mapToCalendarDates(createFeedScopedId(serviceId), netexDao));
+        for (String serviceId : netexDao.getCalendarServiceIds()) {
+            transitBuilder.getCalendarDates().addAll(
+                    mapToCalendarDates(createFeedScopedId(serviceId), netexDao)
+            );
         }
-
-        return transitBuilder;
     }
 }

src/test/java/org/opentripplanner/netex/MappingTest.java
@@ -4,16 +4,17 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.opentripplanner.graph_builder.model.NetexBundle;
-import org.opentripplanner.graph_builder.module.NetexModule;
+import org.opentripplanner.model.Agency;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.impl.OtpTransitServiceBuilder;
+import org.opentripplanner.netex.loader.NetexBundle;
+import org.opentripplanner.netex.loader.NetexLoader;
 import org.opentripplanner.standalone.GraphBuilderParameters;
 import org.opentripplanner.standalone.config.OTPConfiguration;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 // TODO - The mapping needs better tests
 public class MappingTest {
@@ -31,9 +32,14 @@ public class MappingTest {
                 new File(NETEX_DIR, NETEX_FILENAME),
                 new GraphBuilderParameters(buildConfig)
         );
-        NetexModule netexModule = new NetexModule(Collections.singletonList(netexBundle));
-        otpBuilder = netexModule.getOtpDao().stream().findFirst()
-                .orElseThrow(IllegalStateException::new);
+        otpBuilder = new NetexLoader(netexBundle).loadBundle();
+    }
+
+    @Test public void testAgencies() {
+        List<Agency> agencies = otpBuilder.getAgencies();
+
+        // TODO TGR - fix this test
+        Assert.assertEquals(1, agencies.size());
     }
 
     @Test public void testNetexRoutes() {