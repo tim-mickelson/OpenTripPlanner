trnovstyle/OpenTripPlanner-zmaj

Added interchanges

src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
@@ -4,10 +4,10 @@ import org.opentripplanner.netex.loader.NetexDao;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.impl.OtpTransitBuilder;
+import org.opentripplanner.model.Transfer;
 import org.rutebanken.netex.model.Authority;
 import org.rutebanken.netex.model.JourneyPattern;
 import org.rutebanken.netex.model.Line;
-import org.rutebanken.netex.model.Operator;
 import org.rutebanken.netex.model.StopPlace;
 
 import java.util.Collection;
@@ -25,6 +25,8 @@ public class NetexMapper {
     private final TripPatternMapper tripPatternMapper = new TripPatternMapper();
 
     private final OtpTransitBuilder transitBuilder;
+
+    private final TransferMapper transferMapper = new TransferMapper();
 
     private final String agencyId;
 
@@ -62,6 +64,15 @@ public class NetexMapper {
 
         for (String serviceId : netexDao.getCalendarServiceIds()) {
             transitBuilder.getCalendarDates().addAll(mapToCalendarDates(AgencyAndIdFactory.createAgencyAndId(serviceId), netexDao));
+        }
+
+        for (org.rutebanken.netex.model.ServiceJourneyInterchange interchange : netexDao.getInterchanges()) {
+            if (interchange != null) {
+                Transfer transfer = transferMapper.mapTransfer(interchange, transitBuilder, netexDao);
+                if (transfer != null) {
+                    transitBuilder.getTransfers().add(transfer);
+                }
+            }
         }
     }
 }

tim-mickelson/OpenTripPlanner

src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
@@ -2,6 +2,7 @@ package org.opentripplanner.netex.mapping;
 
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.Stop;
+import org.opentripplanner.model.Transfer;
 import org.opentripplanner.model.impl.OtpTransitServiceBuilder;
 import org.opentripplanner.netex.loader.NetexImportDataIndex;
 import org.opentripplanner.netex.support.DayTypeRefsToServiceIdAdapter;
@@ -21,6 +22,7 @@ import static org.opentripplanner.netex.mapping.StopMapper.mapParentAndChildStop
 public class NetexMapper {
 
     private final RouteMapper routeMapper = new RouteMapper();
+    private final TransferMapper transferMapper = new TransferMapper();
     private final TripPatternMapper tripPatternMapper = new TripPatternMapper();
     private final OtpTransitServiceBuilder transitBuilder;
     private final String agencyId;
@@ -60,6 +62,15 @@ public class NetexMapper {
             transitBuilder.getCalendarDates().addAll(
                     mapToCalendarDates(dayTypeRefs, netexIndex)
             );
+        }
+
+        for (org.rutebanken.netex.model.ServiceJourneyInterchange it : netexIndex.interchanges.localValues()) {
+            if (it != null) {
+                Transfer transfer = transferMapper.mapTransfer(it, transitBuilder, netexIndex);
+                if (transfer != null) {
+                    transitBuilder.getTransfers().add(transfer);
+                }
+            }
         }
     }
 }