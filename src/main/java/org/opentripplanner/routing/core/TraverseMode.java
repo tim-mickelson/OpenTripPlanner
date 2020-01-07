b7a6f2098fd62905a6342db96c574529d74eafc1
Gard Mellemstrand <gard.mellemstrand@entur.org>
2020-01-07 12:47:55 +0100

    Add back non-functional traverse modes CAR_DROPOFF and CAR_PICKUP

--- src/main/java/org/opentripplanner/routing/core/TraverseMode.java
+++ src/main/java/org/opentripplanner/routing/core/TraverseMode.java
@@ -13,7 +13,7 @@ public enum TraverseMode {
     TRAM, SUBWAY, RAIL, BUS, FERRY,
     CABLE_CAR, GONDOLA, FUNICULAR,
     TRANSIT, LEG_SWITCH,
-    AIRPLANE;
+    AIRPLANE, CAR_DROPOFF, CAR_PICKUP;
 
     private static HashMap <Set<TraverseMode>, Set<TraverseMode>> setMap = 
             new HashMap <Set<TraverseMode>, Set<TraverseMode>>();
