b0dbd85c92be8f09a19e3a753987a65aacdb7d23
Gard Mellemstrand <gard.mellemstrand@entur.org>
2020-01-27 11:00:07 +0100

    Revert "Enable sandbox transmodel api - TODO Replce this commit with config..."
    This reverts commit 7d5db79942641f194b1bbf25ede4e66032961803.

--- src/main/java/org/opentripplanner/util/OTPFeature.java
+++ src/main/java/org/opentripplanner/util/OTPFeature.java
@@ -24,7 +24,7 @@ public enum OTPFeature {
     // Sandbox extension features - Must be turned OFF by default
     ActuatorAPI(false),
     GoogleCloudStorage(false),
-    SandboxAPITransmodelApi(true),
+    SandboxAPITransmodelApi(false),
     SandboxExampleAPIGraphStatistics(false),
     TransferAnalyzer(false);
 
