f66aedafecbd927409d5b48452d4bdae3f5b2d51
Gard Mellemstrand <gard.mellemstrand@entur.org>
2020-02-03 10:33:56 +0100

    Add dummy includeCancelledTrips argument to estimatedCalls

--- src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelIndexGraphQLSchema.java
+++ src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelIndexGraphQLSchema.java
@@ -1696,6 +1696,12 @@ public class TransmodelIndexGraphQLSchema {
                                 .description("Parameters for indicating the only authorities and/or lines or quays to list estimatedCalls for")
                                 .type(whiteListedInputType)
                                 .build())
+                        .argument(GraphQLArgument.newArgument()
+                            .name("includeCancelledTrips")
+                            .description("Indicates that realtime-cancelled trips should also be included. NOT IMPLEMENTED")
+                            .type(Scalars.GraphQLBoolean)
+                            .defaultValue(false)
+                            .build())
                         .dataFetcher(environment -> {
                             boolean omitNonBoarding = environment.getArgument("omitNonBoarding");
                             int numberOfDepartures = environment.getArgument("numberOfDepartures");
